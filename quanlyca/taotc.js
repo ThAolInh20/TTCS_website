752c0823e48605c33712b001472aed6f591b437b
ThAolInh20 <131834728+ThAolInh20@users.noreply.github.com>
2023-12-04 15:34:51 +0700

    update 12/4/2023

---
+++ quanlyca/taotc.js
@@ -0,0 +1,8 @@
+ocument.addEventListener('DOMContentLoaded', function() {
+    var linkTao = document.getElementById('tao');
+  
+    linkTao.addEventListener('click', function() {
+        alert('Tạo thành công!');
+     
+    });
+});
